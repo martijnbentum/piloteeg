e98d9c33c8310963317a9135a8f7de94e2b42117
Martijn Bentum <bentummartijn@gmail.com>
2016-06-06 12:58:06 +0200

    Define trial, sets filtering on, rerefs the ref to mean (LM RM), no demean, because we will use baseline as a predictor varaible. Load -.2 to 1.0 per marker

--- define_trial.m
+++ define_trial.m
@@ -10,11 +10,12 @@ cfg.dataset                 = pp_filename; %list of filenames
 
 %preprocessing
 
+cfg.continuous = 'no';% if i do not specify this the return of preprocessing is continuous yes, I do not know yet if this is problematic
 %filter wait with filtering
-%cfg.lpfilter        = 'yes';%maybe wait with filtering ?
-%cfg.hpfilter        = 'no';%baseline , should not correlate with epoch
-%cfg.lpfreq          = 30;
-%cfg.hpfreq          = 0.1;% should check whether only 0.5 filter is used
+cfg.lpfilter        = 'yes';%maybe wait with filtering ?I will follow frank et all 2014. Filter first, then filter again at 0.25 0.33 0.5 no addition filter and check for lowest correlation between baseline and window
+cfg.hpfilter        = 'yes';%baseline , should not correlate with epoch
+cfg.lpfreq          = 30;%this was standard in my analysis, and the most common in my literature study
+cfg.hpfreq          = 0.1;% most common in my literature study
 
 %referencing
 cfg.reref           = 'yes';  % Rereference
