3f47481d88c2f9365b0e583b54c2d07c1cc35b21
Martijn Bentum <bentummartijn@gmail.com>
2016-06-07 15:39:53 +0200

    filter_check compares different high pass frequencies with filterorder 4 (highest allowable) to the default (with 1hz, lowest possible high pass) and firws at default, for 0.1hz the butterworth order 4 0.05 seems best. HOWEVER. Luck ch5 advises against high pass filters. So I will only use low pass while reading in data and only later on do a high pass to decorrelate baseline with window.

--- filter_check.m
+++ filter_check.m
@@ -1,20 +1,26 @@
 function output = filter_check()
 
+output = [];
 % generate the signal and the corresponding time axis
 s = zeros(1,1000); s(500) = 1; s = s - mean(s);
 t = 0.001:0.001:1;
 
-str = 'compare different filter orders (Butterworth)';
+str = 'compare dif filters and orders (Butterworth firws)';
 clear f
-f(1,:) = s;
-f(2,:) = ft_preproc_lowpassfilter(s, 1000, 50,4,'but');
-f(3,:) = ft_preproc_lowpassfilter(s, 1000, 50,2); 
-f(4,:) = ft_preproc_lowpassfilter(s, 1000, 50,3);
+%f(1,:) = s;
+f(1,:) = ft_preproc_lowpassfilter(s, 1000, 1,[],'but');
+f(2,:) = ft_preproc_lowpassfilter(s, 1000, 0.05,4,'but');
+f(3,:) = ft_preproc_lowpassfilter(s, 1000, 0.1,4); 
+f(4,:) = ft_preproc_lowpassfilter(s, 1000, 0.5,4,'but');
+f(5,:) = ft_preproc_lowpassfilter(s, 1000, 0.1,[],'firws');
 
 figure; 
-subplot(1,2,1); 
-plot(t, f-repmat((0:3)',1,1000)); grid on; set(gca, 'ylim', [-3.5 0.5]); xlabel('time (s)'); ylabel(str);
-output = subplot(1,2,2); 
-semilogy(abs(fft(f, [], 2).^2)'); grid on; set(gca, 'xlim', [0 500]); xlabel('freq (Hz)'); ylabel(str);
-
-print -dpng fig7.png
+%subplot(1,2,1); 
+%plot(t, f-repmat((0:3)',1,1000)); grid on; set(gca, 'ylim', [-3.5 0.5]); xlabel('time (s)'); ylabel(str);
+%subplot(1,2,2); 
+output.h = semilogy(abs(fft(f, [], 2).^2)'); grid on; set(gca, 'xlim', [0 9]); xlabel('freq (Hz)'); ylabel(str);
+legend('1hz default but','0.05hz order4 but','0.1hz order4 but','0.5hz order4 but','0.1hz dfault firws');
+print -dpng filter_comparison.png
+output.f = f;
+output.t =t;
+output.s =s;
