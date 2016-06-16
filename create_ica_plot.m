0c69a64bd72dcafc2695b6218b3f89190099b581
Martijn Bentum <bentummartijn@gmail.com>
2016-06-16 13:09:40 +0200

    changed ica topoplot layout file to eeg1010 for 64 channels, changed fieldtrip files topoplot common to plot correlation beneath each plot with eog channels, should fill in cfg.commments = cell and cfg.comment = cell to properly show the correlation

--- create_ica_plot.m
+++ create_ica_plot.m
@@ -1,7 +1,11 @@
 function output = create_ica_plot(d)
 
 set(gcf,'Visible','off');
-
+x0=10;
+y0=10;
+width=1200;
+height=1080;
+set(gcf,'units','points','position',[x0,y0,width,height])
 
 len = length(d.label)
 
@@ -14,13 +18,14 @@ end
 %h = figure
 cfg = [];
 cfg.component = [1:len];       % specify the component(s) that should be plotted
-cfg.layout    = 'easycapM23.mat'%'EEG1010.lay'; % specify the layout file that should be used for plotting
+cfg.layout    = 'EEG1010'%'easycapM23.mat'%'EEG1010.lay'; % specify the layout file that should be used for plotting
 cfg.comments = 'cells' % !!!!!!!!! -> modified fieltrip file,normal fieldname is comment: fieldtrip-2016xxx/private/topoplot_common.m line 673 674, added the following if statement
+cfg.comment = 'cells'%
 %elseif strcmp(cfg.comments, 'cells')  cfg.comment = cfg.commentcell(cfg.component);
-cfg.commentpos = 'leftbottom;%change topoplot_common to -0.6 -0.9 for better position, copy of
+cfg.commentpos = 'leftbottom';%change topoplot_common to -0.6 -0.9 for better position, copy of
 %topoplot_common can be found in piloteeg directory
 cfg.commentcell = com_cell
-set(gcf,'Visible','off');
+%set(gcf,'Visible','off');
 
 ft_topoplotIC(cfg, d)
 
@@ -29,6 +34,6 @@ ft_topoplotIC(cfg, d)
 %easycapM23.mat
 %'~/Documents/MATLAB/EEG_Martijn/'
 %output = strcat(d.filename,'_plot')
-print('-dpng',strcat(d.filename,'_plot'))
+print('-dpng',strcat(d.filename,'_plot.png'))
 
 %close(h)
