be7be53e47654f5759aa0ade952b71e7446742ef
Martijn Bentum <bentummartijn@gmail.com>
2016-07-04 10:48:06 +0200

    create averages, added code to calculate the average of lower quantile and highest quantile of word logprobs

--- create_averages.m
+++ create_averages.m
@@ -13,11 +13,32 @@ for day_index = 1:2
 		fn = getfield(pp,strjoin(day(day_index)),strjoin(red(r)));
 		disp(strcat(day(day_index),'-',red(r)))
 		for i = 1:length(fn.file_id)
-			disp(strcat(fn.file_id(i),'_clean.mat'))
-			load(strcat(strjoin(fn.file_id(i)),'_clean.mat'))
-			cfg = getfield(fn, strjoin(fn.file_id(i)))
-			d = remove_trials(cfg.all_trl_remove,d);			
-			
+		%for each pp in condition calculate the average (this is not demeaned, but you can do this afterwards
+			disp(strcat(fn.file_id(i),'_clean.mat'))%show filename
+			load(strcat(strjoin(fn.file_id(i)),'_clean.mat'))%load file
+			cfg = getfield(fn, strjoin(fn.file_id(i)))%extract pp info
+
+			%extract all trials with open class words
+			d1 = remove_trials(cfg.rm_trl_word_condition,d);%use the specified trl from select trials to select trials	
+
+			%calc all open class word average
+			open_class_avg = ft_timelockanalysis([],d1);%create average	
+			pp = setfield(pp,strjoin(day(day_index)),strjoin(red(r)),strjoin(fn.file_id(i)),'open_class_avg',open_class_avg);%add the pp avg to the datastructure of the specific participant 
+			%calc lower_q average
+			not_lower_q = setdiff(1:length(d.trialinfo),cfg.lower_q_trials);
+			remove_trl_lower_q = unique([not_lower_q cfg.rm_trl_word_condition]);
+			d1 = remove_trials(remove_trl_lower_q,d);%use the specified trl from select trials to select trials	
+			lower_q_avg = ft_timelockanalysis([],d1);
+			pp = setfield(pp,strjoin(day(day_index)),strjoin(red(r)),strjoin(fn.file_id(i)),'lower_q_avg',lower_q_avg);%add the pp avg to the datastructure of the specific participant 
+
+			%calc higher_q average
+			not_higher_q = setdiff(1:length(d.trialinfo),cfg.higher_q_trials);	
+			remove_trl_higher_q = unique([not_higher_q cfg.rm_trl_word_condition]);
+			d1 = remove_trials(remove_trl_higher_q,d);%use the specified trl from select trials to select trials	
+			higher_q_avg = ft_timelockanalysis([],d1);
+			pp = setfield(pp,strjoin(day(day_index)),strjoin(red(r)),strjoin(fn.file_id(i)),'higher_q_avg',higher_q_avg);%add the pp avg to the datastructure of the specific participant 
+
+
 		end
 	end
 end
