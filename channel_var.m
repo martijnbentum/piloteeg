4e823426c87562ad568966b1c881d1c4165e1ef2
Martijn Bentum <bentummartijn@gmail.com>
2016-07-05 19:35:31 +0200

    channel_var changed input variable, is concatenated trial matrix: channels by all timepoints (of the concatenated trials)

--- channel_var.m
+++ channel_var.m
@@ -1,17 +1,21 @@
 function var_info = channel_var(d)
 %returns the variance of each channel
-%assumes d contains a trial field with 1 cell, the eeg data read in continuously
+%assumes d to be a struct with m a matrix, rows=channels, columns=timepoints
 %computes the variance for each channel and calculates a zscore for the variance of each channel
-%labels of channel with a zscore > 3 are stored in bad_channels
-%could be removed with ft_channel_repair
+%d.label shouls contain the label of each channel, function will label of channel with a zscore > 3 are stored in bad_channels
+%bad channels could be removed with ft_channel_repair, which uses interpolate
 
-
-cvar = var(d.trial{1}');
+%output contains channel_variance, zchannel_variance (zscore) and bad_channels, channels with a abs(z-score) > 3
+cvar = var(d.m');
 zcvar = zscore(cvar);
-bad_channels = d.label{find(zcvar > 3)}
+if ~isempty(d.label(find(abs(zcvar) > 3)))
+	bad_channels = d.label(find(abs(zcvar) > 3));%not the brackets should be () to be able to extract multiple elements from cell array d.label 
+else
+	bad_channels = [];
+end
 
 var_info = [];
 var_info.cvar = cvar;
 var_info.zcvar = zcvar;
-var_info.bad_channels = bad_channels;
+var_info.var_bad_channels = bad_channels;
 
