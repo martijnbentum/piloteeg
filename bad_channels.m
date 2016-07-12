1cc7a0bf79218506d6d2d8ac1a89d9d8eacba7ac
Martijn Bentum <bentummartijn@gmail.com>
2016-07-12 10:38:20 +0200

    updated variable names

--- bad_channels.m
+++ bad_channels.m
@@ -1,9 +1,9 @@
-function [n_bad_var_ch,n_bad_cor_ch,n_bad_ch,bad_ch] = bad_channels(channel);
+function [n_bad_var_ch,n_bad_cor_ch,n_bad_ch,bad_ch] = bad_channels(garbage);
 %extract info from channel statistics, original in a pp specific garbage.mat file
 
-n_bad_var_ch = length(channel.var.var_bad_channels);
-n_bad_cor_ch = length(channel.cor.corr_bad_channels);
-bad_ch = unique([channel.var.var_bad_channels' channel.cor.corr_bad_channels']);
+n_bad_var_ch = length(garbage.channel_var.var_bad_channels);
+n_bad_cor_ch = length(garbage.channel_cor.corr_bad_channels);
+bad_ch = unique([garbage.channel_var.var_bad_channels' garbage.channel_cor.corr_bad_channels']);
 n_bad_ch = length(bad_ch);
 
 %concatenate channel labels with ',', because writing to file does not accept cells
