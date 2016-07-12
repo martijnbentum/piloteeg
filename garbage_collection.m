51129a47b30f95e2373e1d4051bed556ca3f20b8
Martijn Bentum <bentummartijn@gmail.com>
2016-07-12 10:42:31 +0200

    garbage collection, generalized code, now take cfg to select options, eg exclude blink detection because it is time intensive

--- garbage_collection.m
+++ garbage_collection.m
@@ -1,10 +1,17 @@
-function garbage = garbage_collection(d)
+function garbage = garbage_collection(cfg,d)
 %executes several functions to compute garbage statistics: variance of channels, correlations of channels and epoch checks:thresholds, variance, epoch checks should be done on data seperated in shorter segments so first create snippets of data of 1 second
 %devide all trials into 1 second streches for blink detection
 
+if ~isfield(cfg,'eog_channel'), cfg.eog_channel = {'all','-eogv','-eogh','-RM','-REF_LM','-Fp1','-Fp2'}; end%
+		  if ~isfield(cfg,'channel'), cfg.channel = {'all'}; end%,'-AF7','-F7','-FT7','-P7','-PO7','-O1','-O2','-PO8','-P8','-FT8','-F8','-AF8'}; end %
+if ~isfield(cfg,'check_blinks'), cfg.check_blinks = 'yes';end%default to check for blinks
+if ~isfield(cfg,'check_channel'), cfg.check_channel = 'yes';end %default to check channels (var and correlation) 
+if ~isfield(cfg,'check_epoch'),cfg.check_epoch = 'yes'; end%default to check epoch (deviance variance amp range)
+
+
 %create variable that collects all garbage statistics
 garbage = [];
-
+garbage.cfg = cfg;
 
 %EPOCH CHECK
 
@@ -13,42 +20,65 @@ c = [];
 c.length = 1; %seconds
 c.overlap = 0;% no overlap
 snips = ft_redefinetrial(c,d);
+garbage.epoch_trial_select = c;
+garbage.epoch_trl = [snips.sampleinfo zeros(size(snips.sampleinfo,1),1)];
 
-%blinks, they are simplistically defined as epoch exceeding threshold of +- 75 mV
-%-----
-%trl structure should minimally contain 3 columns start end baseline offset:create 3 column for baseline offset
-%adding a zero column to sample info to comply
-trl = [snips.sampleinfo zeros(size(snips.sampleinfo,1),1)];
-blinks = get_blink_trial(trl,snips)
-%remove unnecessary fields for data reduction
-fields = {'hdr','trial','time','trialinfo','cfg'};
-blinks = rmfield(blinks,fields);
-blinks.trl = trl;
-garbage.blinks = blinks;
+if strcmp(cfg.check_blinks, 'yes')
+	%blinks, they are simplistically defined as epoch exceeding threshold of +- 75 mV
+	%-----
+	%trl structure should minimally contain 3 columns start end baseline offset:create 3 column for baseline offset
+	%adding a zero column to sample info to comply
+	blinks = get_blink_trial(garbage.epoch_trl,snips)
+	%remove unnecessary fields for data reduction
+	fields = {'hdr','trial','time','trialinfo','cfg'};
+	blinks = rmfield(blinks,fields);
+	garbage.blinks = blinks;
+else
+	garbage.blinks = 'not_checked'
+end
 
+
+if strcmp(cfg.check_epoch, 'yes')
+	%remove channels from dataset
+	c = [];
+	c.channel = [cfg.eog_channel cfg.channel];
+	snips = ft_selectdata(c,snips);
+	garbage.epoch_ch_label = snips.label
+	%epoch statistics, based on Nolan et al 2010 (FASTER)
+	garbage.epoch_var = epoch_variance(snips);
+	garbage.epoch_dev = epoch_deviation(snips);
+	garbage.epoch_amp = epoch_amplitude_range(snips);
+	garbage.epoch_ch_select = c;
+end
 %could possible benefit from epoch checks as well, but i am not sure
 
 
 %CHANNEL CHECK
-channel = [];
-%for these calculations the eog channels should be removed, because they could mask noise in the scalp channels,this is because zscore of 3 is an exclusion threshold (Nolan et al. 2010), and the noise in eog channels makes it harder for scalp channel to be an outlier.
-cfg = [];
-cfg.channel = {'all','-eogv','-eogh','-RM','-REF_LM','-Fp1','-Fp2'};%
-d= ft_selectdata(cfg,d);
+if strcmp(cfg.check_channel, 'yes')
+	channel = [];
+	%for these calculations the eog channels should be removed, because they could mask noise in the scalp channels,this is because zscore of 3 is an exclusion threshold (Nolan et al. 2010), and the noise in eog channels makes it harder for scalp channel to be an outlier.
+	c = [];
+	c.channel = [cfg.eog_channel cfg.channel];%select channels that are given in the default or by user in channel and cfg.channel
+	d= ft_selectdata(c,d);
+	
+	%concatenate all trials for channels statistic calculation
+	temp = [];
+	temp.m = concatenate_trials(d);
+	temp.label = d.label;
+	
+	garbage.channel_ch_label = d.label
+	%channel variance
+	%----------------
+	garbage.channel_var = channel_var(temp);
+	
+	%channel mean pairwise correlation
+	%---------------------------------
+	%channel mean correlation: mean of the correlation of each channel paired with every other channel
+	garbage.channel_cor = channel_cor(temp);
+	
+	garbage.channel_ch_select = c
+end
 
-%concatenate all trials for channels statistic calculation
-temp = [];
-temp.m = concatenate_trials(d);
-temp.label = d.label;
 
-%channel variance
-%----------------
-channel.var = channel_var(temp);
 
-%channel mean pairwise correlation
-%---------------------------------
-%channel mean correlation: mean of the correlation of each channel paired with every other channel
-channel.cor = channel_cor(temp);
-
-garbage.channel = channel;
 
