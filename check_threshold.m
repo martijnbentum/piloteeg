5e83b8a2fd8c351416a33965bed16102dd29492a
Martijn Bentum <bentummartijn@gmail.com>
2016-07-20 10:22:57 +0200

    check_threshold, updates for no_overlap, now works with story version files

--- check_threshold.m
+++ check_threshold.m
@@ -1,20 +1,25 @@
 function preproc_cfg = check_threshold(d)
 
-p_name = d.filename;
+%obsolete, after redefine trial no longer present
+%p_name = d.filename;
+%disp(p_name);
 
 
 %OBSOLETE, eog and mastoid channels are not present anymore
 %cfg.channel = {'all','-eogv','-RM','-eogh','-REF_LM','-Fp1'} ;
 %cfg.trials = 1:100
 %d        = ft_selectdata(cfg, d); 
-disp(p_name);
-preproc_cfg = load(strcat(p_name(1:7),'_cfg.mat'))
+%obsolet, could be used with cfgstories.mat, however no word trl definition present anyway
+%preproc_cfg = load(strcat(p_name(1:7),'_cfg.mat'))
 cfg = [];
 
-bt = [d.eogh_bad_trials d.eogv_bad_trials];%concatenate all bad trials
-bt = unique(bt);
+%obsolete in the no overlap version
+%bt = [d.eogh_bad_trials d.eogv_bad_trials];%concatenate all bad trials
+%bt = unique(bt);
 
-cfg.trl = preproc_cfg.d.trl;
+%obsolete cfg.d.trl is not defined for words
+%cfg.trl = preproc_cfg.d.trl;
+d.trl = [d.sampleinfo zeros(size(d.sampleinfo,1),1)];
 cfg.continuous = 'no';
 cfg.artfctdef.threshold.min       = -75;
 cfg.artfctdef.threshold.max       = 75;
@@ -24,8 +29,10 @@ cfg.artfctdef.threshold.bpfilter = 'no';
 
 [con, artifact] = ft_artifact_threshold(cfg,d);
 
-preproc_cfg.d.bad_trials = bt;
-preproc_cfg.d.rejected_trials= sample2trln(artifact,d.sampleinfo);%con.rejected_trials
+%preproc_cfg.d.bad_trials = bt;
+preproc_cfg.d.threshold_rejected_trials= sample2trln(artifact,d.sampleinfo);%con.rejected_trials
+preproc_cfg.d.threshold_rejected_sampleinfo = artifact;
+preproc_cfg.d.threshold_ntrials = length(d.trial);
 preproc_cfg = preproc_cfg.d;
 
 
