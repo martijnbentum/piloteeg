f8af0da45560744d522d0bf52024b51735a5f9b5
Martijn Bentum <bentummartijn@gmail.com>
2016-07-20 10:26:54 +0200

    plot_clean_preproc, now plots horizontal and vertical blink trial\n however story word trial conversion and lack of baseline/ and or filter on trial level gives some problems with blink detection

--- plot_clean_preproc.m
+++ plot_clean_preproc.m
@@ -2,15 +2,17 @@ function output = plot_clean_preproc(pp)
 
 p_name = pp.file_id;
 clean = strcat(p_name,'_clean.mat')
-preproc = strcat(p_name,'_preproc.mat')
+preproc = strcat(p_name,'_reject.mat')
 load(clean)
 clean1 = ft_selectdata(d,'channel',{'Fz','Cz'})
+clean1 = ft_redefinetrial(clean1.cfg_redefine,clean1);
 
 load(preproc)
 preproc1 = ft_selectdata(d,'channel',{'Fz','Cz'})
+preproc1 = ft_redefinetrial(preproc1.cfg_redefine,preproc1);
 
-for p = 1 : 5
-    trial = clean1.eogv_bad_trials(p)
+for p = 1 : 3
+    trial = pp.eogv_bad_trials(p)
     clf
     %hold on
 
@@ -22,10 +24,31 @@ for p = 1 : 5
         ylim([-60 60])
 %    end
     
-    legend('clean','preproc');
+    legend('clean','reject');
     title(strcat(p_name,' Channel:  ',clean1.label(i)))
     %ylim([0 100])
 	 end
-    print('-dpng',strcat(p_name,'_plot_clean_preproc_test',num2str(trial)))
+    print('-dpng',strcat(p_name,'_plot_clean_reject_vertical',num2str(trial)))
     clf
+
+    trial = pp.eogh_bad_trials(p)
+    clf
+    %hold on
+
+    set(gcf, 'Visible', 'off')
+    
+    for i = 1:2
+		  subplot(1,2,i)
+        plot(clean1.time{trial}(1,:),clean1.trial{trial}(i,:),preproc1.time{trial}(1,:),preproc1.trial{trial}(i,:))
+        ylim([-60 60])
+%    end
+    
+    legend('clean','reject');
+    title(strcat(p_name,' Channel:  ',clean1.label(i)))
+    %ylim([0 100])
+	 end
+    print('-dpng',strcat(p_name,'_plot_clean_reject_horizontal',num2str(trial)))
+    clf
+
+
 end
