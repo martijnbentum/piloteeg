22093a281f30c45a1ecf57889a316c19eedefe5e
Martijn Bentum <bentummartijn@gmail.com>
2016-08-31 13:01:25 +0200

    channel_interpolate, added neighbour deletion if neighbour = bad_channel saves neighbour info and number of deleted neighbours

--- channel_interpolate.m
+++ channel_interpolate.m
@@ -40,26 +40,66 @@ z = [];
 if ~isfield(cfg,'badchannel')
 	bc = load(strcat(d.filename(1:7),'_cfgchannel.mat'))
 	[n_bad_var_ch,n_bad_cor_ch,n_bad_ch,bad_ch] = bad_channels(bc.d);	
-	cfg.badchannel = bad_ch;
+	cfg.badchannel = strsplit(bad_ch,',');
 end
 
 z.neighbours = cfg.neighbours;
 z.badchannel = cfg.badchannel;
 z.layout = cfg.layout;
 %--------------------------------------------------
+neighbour_removed = 0;                                                                    
 
+messages = {};
+neighbours = struct([]);
 
+if ~strcmp(z.badchannel,'none')
+	%save neighbours of the bad channels
+	labels = {z.neighbours.label}
+	for i = 1:length(z.badchannel)
+		index = strmatch(z.badchannel(i),labels)
+		neighbours = [neighbours z.neighbours(index)]
+	end
 
+	if length(z.badchannel) > 1
+
+		for i = 1 : length(neighbours)
+			for j = 1 : length(z.badchannel)
+				t = 0;
+				for k = 1 : length(neighbours(i).neighblabel)
+					if strcmp(neighbours(i).neighblabel(k - t) , z.badchannel(j))
+						m = strcat('removing neighbour: ',z.badchannel(j),' from badchannel: ',neighbours(i).label);
+						messages = [messages m];
+						disp(m)
+						neighbours(i).neighblabel(k) = [];
+						neighbour_removed = neighbour_removed + 1;
+						t = t + 1;
+					end
+				end
+			end
+		end
+	end
+end
 %--------------------------------------------------
 %channel interpolation, default method is weighted ?average?
 %--------------------------------------------------
 %only if there are bad channels
 %--------------------------------------------------
 if ~strcmp(z.badchannel,'none')
-	d = ft_channelrepair(z,d); 
+	d = ft_channelrepair(z,d);
+
+	%save neighbours of the bad channels
+	neighbours = struct([]);
+	labels = {z.neighbours.label}
+	for i = 1:length(z.badchannel)
+		index = strmatch(z.badchannel(i),labels)
+		neighbours = [neighbours z.neighbours(index)]
+	end
 end
 %store interpolated_channels in datastructure
 d.interpolated_channels = z.badchannel
+d.neighbour_removed.n = neighbour_removed;
+d.neighbour_removed.info = messages;
+d.neighbour = neighbours;
 %--------------------------------------------------
 
 
