e4bd118a50730fa92dfd875da5bb48fbb0ba97b3
Martijn Bentum <bentummartijn@gmail.com>
2016-09-05 13:13:22 +0200

    select_trails.m, changed filename for alternate analysis

--- select_trials.m
+++ select_trials.m
@@ -1,6 +1,11 @@
 function  pp = select_trials(pp)
 %use the pp structure that holds pp according to day (1,3) and reduction (reduced, unreduced) created in make_pp_groups. THis function adds fields for each participant in a specific condition (name = file_id) and stores the trials to be removed all_trl_remove, this combines trials threshold rejected and trials that are closed class (it leaves all open class words. Futher info: filename of the clean file (after ica correction) n trials
 
+
+d_filename = '_clean_word_interpolate_alt.mat'
+cfg_threshold = '_threshold_clean_wordcfg_alt.mat'
+
+
 reduced = [1 2];
 day_id = [1 3];
 red = {'unreduced' 'reduced'};
@@ -14,15 +19,15 @@ for day_index = 1:2
 		fn = getfield(pp,strjoin(day(day_index)),strjoin(red(r)));
 		disp(strcat(day(day_index),'-',red(r)))
 		for i = 1:length(fn.file_id)
-			disp(strcat(fn.file_id(i),'_clean.mat'))
+			disp(strcat(fn.file_id(i),d_filename))
 			
 			%load d file and cfg file; d file is for trialinfo and cfg is for rejected
-			load(strcat(strjoin(fn.file_id(i)),'_clean.mat'))
-			cfg_clean = load(strcat(strjoin(fn.file_id(i)),'_cfg.mat'));
+			load(strcat(strjoin(fn.file_id(i)),d_filename))
+			cfg_clean = load(strcat(strjoin(fn.file_id(i)),cfg_threshold));
 
 			%create cfg for create condition
 			cfg = [];
-			cfg.filename = strcat(strjoin(fn.file_id(i)),'_clean.mat');
+			cfg.filename = strcat(strjoin(fn.file_id(i)),d_filename);
 			cfg.n_orignal_trl = length(d.trial);
 			cfg = setfield(cfg,'reduced',reduced(r));
 			cfg = setfield(cfg,'day_id',day_id(day_index));
@@ -38,7 +43,7 @@ for day_index = 1:2
 			cfg = create_condition(cfg,d);
 
 			%add extra info to the file id struct
-			cfg.rejected_trials = cfg_clean.d.rejected_trials;
+			cfg.rejected_trials = cfg_clean.d.threshold_rejected_trials;% 01/09/16 12:55,added threshold
 			cfg.rm_trl_word_condition = unique([cfg.closed_class_trials cfg.pseudoword_trials cfg.rejected_trials]);
 
 			%save it to the pp struct
