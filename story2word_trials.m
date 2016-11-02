39e70dd4c866de58fba6bf6f087bb63dc3a9bf3d
Martijn Bentum <bentummartijn@gmail.com>
2016-11-02 12:16:19 +0100

    story2wordtrials made filtering optional

--- story2word_trials.m
+++ story2word_trials.m
@@ -5,22 +5,25 @@ function output = story2word_trials(cfg,d)
 %cleans up resulting d with info from the original d (ft_redefinetrial hides original info)
 
 %set defaults if necessary, hpfreq can be set to find filter freq with lowest cor between baseline and erp window
+if ~isfield(cfg,'hpfilter'),cfg.hpfilter = 'yes';end
 if ~isfield(cfg,'hpfreq'),cfg.hpfreq = 0.05; end
 if ~isfield(cfg,'hpfiltord'),cfg.hpfiltord = 4;end
-if ~isfield(cfg,'redefine'),cfg.redefine = d.redefine;
+if ~isfield(cfg,'redefine'),cfg.redefine = d.redefine;end
 
 %create word trials based on the cfg redefine structure
 output = ft_redefinetrial(cfg.redefine,d);
 
 
 %create config structure for  preprocessing
-c.hpfilter = 'yes';
-c.hpfreq = cfg.hpfreq;
-c.hpfiltord = cfg.hpfiltord;
-disp('filtering with following config file (ft_preprocessing)');
-c
-%hp filter the word trials
-output = ft_preprocessing(c,output);
+if strcmp(cfg.hpfilter,'yes')
+	c.hpfilter = cfg.hpfilter;
+	c.hpfreq = cfg.hpfreq;
+	c.hpfiltord = cfg.hpfiltord;
+	disp('filtering with following config file (ft_preprocessing)');
+	c
+	%hp filter the word trials
+	output = ft_preprocessing(c,output);
+end
 
 
 %restore info from original d file
